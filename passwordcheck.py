ccf58cb62eee11515373b394d7472a29b1e45bf3
PythonUserNo <95596419+PythonUserNo@users.noreply.github.com>
2021-12-06 11:28:50 +0700

    passwordcheck.py

---
+++ passwordcheck.py
@@ -0,0 +1,12 @@
+
+print("MANUFACTURED AND CODED BY THE NCR-SPECIALIST SQUADRON IN-JOINT WITH VAULT-TEC")
+while True:
+
+    password = input("ENTER PROVIDED SECURITY CODE:     ")
+    if password == "g7ACubS5U*zN@YVt0iKN7i":
+        print("Access Granted")
+
+        break
+
+    else:
+        print("Access Denied")
